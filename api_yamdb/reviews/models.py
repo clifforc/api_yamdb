b9c69662e17dee6a4f94239c98b5d41b5c772a08
Кирилл Мансуров <kirill.mansurov.2002@mail.ru>
2024-07-31 12:55:01 +0300

    create model Title, Categories, Genres

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -16,3 +16,30 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    genre = models.ForeignKey(
+        Genres, on_delete=models.CASCADE, related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
