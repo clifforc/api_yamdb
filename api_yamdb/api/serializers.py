e854b68e426a17eb8e4866d05b8f4b8bfd4aab0b
clifforc <clifforc@student.21-school.ru>
2024-07-31 21:17:21 +0300

    Добавлен SignUpSerializer и GetTokenSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,3 +8,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name',
                   'bio', 'role')
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username',)
+
+
+class GetTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'confirmation_code')
