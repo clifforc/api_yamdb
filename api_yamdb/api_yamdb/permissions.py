30ff26d8d3d9e92acf0ea53a1e7074882be4f95b
clifforc <clifforc@student.21-school.ru>
2024-07-31 09:54:08 +0300

    Added IsAdmin and IsAuthorModeratorAdminOrReadOnly user permissions classes to api/permissions.py, also added IsAuthenticated default permission to settings.py

---
+++ api_yamdb/api_yamdb/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+
+class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object or
+    moderators/admins to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return (obj.author == request.user or
+                    request.user.role == 'moderator' or
+                    request.user.role == 'admin')
+
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Custom permission to only allow access to users with the 'admin' role.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'admin'
