b35e70bda113e5df6a13edc449432cce78027757
clifforc <clifforc@student.21-school.ru>
2024-08-01 14:20:15 +0300

    added IsAdmin permission and UserViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .permissions import IsAdmin
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdmin,)
