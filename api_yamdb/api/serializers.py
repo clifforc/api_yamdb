0f4a5ceb25e58ae7ea42a110e7262fb484865549
Oleinikov <j-n-d@yandex.ru>
2024-08-01 15:29:36 +0300

    add serializers and viewsets

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from reviews.models import Comment, Review
+
 
 User = get_user_model()
 
@@ -9,3 +13,32 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name',
                   'bio', 'role')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate(self, data):
+        user = self.context.get('request').user
+        title = self.instance.title if self.instance else data.get('title')
+        if user.reviews.filter(title=title).exists():
+            raise ValidationError(
+                'Вы уже оставляли отзыв на это произведение.'
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
